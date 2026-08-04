ba205594e6faf295562fca62a69ddda91286b147
tintinaccs <tintinaccs@gmail.com>
2026-08-04 09:11:12 -0300

    refactor(navigation): isolate shared surface layer

---
+++ js/components/navigation/shared/surface-layer.js
@@ -0,0 +1,5 @@
+export function renderSurfaceLayer() {
+  return `
+    <div class="tt-shared-backdrop" id="tt-shared-backdrop" hidden></div>
+    <div class="tt-shared-morph" id="tt-shared-morph" aria-hidden="true"></div>`;
+}
