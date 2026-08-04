206b76d236f97637d41bdc64418e52fcc4754fc2
tintinaccs <tintinaccs@gmail.com>
2026-08-04 09:08:40 -0300

    refactor(navigation): add shared modular config

---
+++ js/components/navigation/shared/config.js
@@ -0,0 +1,27 @@
+export const SHELL_VERSION = 'tintin-20260804-modular-shell-1';
+
+export const BREAKPOINTS = Object.freeze({
+  mobileMax: 767,
+  tabletMin: 768,
+  tabletMax: 1024,
+  desktopMin: 1025,
+});
+
+const SITE_ROOT_URL = new URL('../../../../', import.meta.url);
+const JS_ROOT_URL = new URL('../../../', import.meta.url);
+
+export function versionedSiteAsset(path) {
+  const url = new URL(path, SITE_ROOT_URL);
+  url.searchParams.set('v', SHELL_VERSION);
+  return url.href;
+}
+
+export function versionedJsModule(path) {
+  const url = new URL(path, JS_ROOT_URL);
+  url.searchParams.set('v', SHELL_VERSION);
+  return url.href;
+}
+
+export function logoUrl() {
+  return versionedSiteAsset('assets-tintin/images/general/logo.png');
+}
