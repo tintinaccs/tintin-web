65604b24975caf38c4bcd10fb790eb5468e40990
tintinaccs <tintinaccs@gmail.com>
2026-08-15 11:03:14 -0300

    Bump global visual runtime after offset fix

--- js/components/navigation/entrada-navegacion-publica.js
+++ js/components/navigation/entrada-navegacion-publica.js
@@ -12,7 +12,7 @@ import { loadSharedRuntime } from './compartido/carga-navegacion.js';
 import { enhanceMobileFooter } from './compartido/acordeon-pie-pagina.js';
 import { registerNavigationSurfaces } from './compartido/registro-paneles.js';
 import { fetchGlobalVisualStudioConfig, applyGlobalLayout } from './compartido/apariencia-global.js?v=tintin-20260815-prelaunch-cache-1';
-import { applyGlobalVisualStudio } from '../../core/store/visual-studio-global-runtime.js?v=tintin-20260810-global-studio-9';
+import { applyGlobalVisualStudio } from '../../core/store/visual-studio-global-runtime.js?v=tintin-20260815-global-studio-10';
 
 const LEGACY_SHELL_IDS = Object.freeze([
   'tt-header-desktop-tablet',
