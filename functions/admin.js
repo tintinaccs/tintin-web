ff8b52e54c4d4d0a6961be647606da7b085c40b4
tintinaccs <tintinaccs@gmail.com>
2026-08-17 10:13:36 -0300

    fix(admin): versionar runtime maestro en /admin

--- functions/admin.js
+++ functions/admin.js
@@ -1,7 +1,9 @@
 import { serveAdminWithCsp } from '../cloudflare/servir-admin-con-csp.js';
 import { injectMasterDiagnosticsRuntime } from '../cloudflare/inyectar-diagnostico-maestro-admin.js';
 
+const MASTER_DIAGNOSTICS_RUNTIME = '/js/admin/diagnostics/diagnostico-maestro-admin.js?v=tintin-20260817-master-diagnostics-2';
+
 export async function onRequest(context) {
   const response = await serveAdminWithCsp(context, 'admin');
-  return injectMasterDiagnosticsRuntime(response, context.request.method);
+  return injectMasterDiagnosticsRuntime(response, context.request.method, MASTER_DIAGNOSTICS_RUNTIME);
 }
