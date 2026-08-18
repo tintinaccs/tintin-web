152a96a3564c17eb7c32e14f493e349eb8bf1dc0
tintinaccs <tintinaccs@gmail.com>
2026-08-18 08:33:42 -0300

    chore(shell): versionar navegación responsive y notificaciones realtime

--- js/components/navigation/compartido/configuracion.js
+++ js/components/navigation/compartido/configuracion.js
@@ -1,4 +1,4 @@
-export const SHELL_VERSION = 'tintin-20260818-header-dropdowns-solid-3';
+export const SHELL_VERSION = 'tintin-20260818-responsive-shell-realtime-1';
 
 export const BREAKPOINTS = Object.freeze({
   mobileMax: 767,
