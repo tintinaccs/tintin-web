075203724c442f279fb1dbab3d53e4b38234f5b0
tintinaccs <tintinaccs@gmail.com>
2026-08-27 15:36:10 -0300

    refactor: centralizar configuración del puente Sheets

---
+++ cloudflare/sheets-sync-config.js
@@ -0,0 +1,4 @@
+export const APPS_SCRIPT_SYNC_URL = 'https://script.google.com/macros/s/AKfycbyh9I5aPp9d3lMSnYRNfrHcSCCobCoDOif9CqtXmMe4FgwSjzlKf4kjQZqvKDRmEY6S/exec';
+export const SHEETS_TIMEOUT_MS = 12_000;
+export const SHEETS_HEALTH_TIMEOUT_MS = 5_000;
+export const SHEETS_HEALTH_REVISION = 'sheets-health-v1';
