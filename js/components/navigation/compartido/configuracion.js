417eedcd77e39999c6a679a2a22a3084cb9fde07
tintinaccs <tintinaccs@gmail.com>
2026-08-31 09:51:35 -0300

    fix: versionar runtime modular de producto

--- js/components/navigation/compartido/configuracion.js
+++ js/components/navigation/compartido/configuracion.js
@@ -1,4 +1,4 @@
-export const SHELL_VERSION = 'tintin-20260825-responsive-css-budget-2';
+export const SHELL_VERSION = 'tintin-20260831-product-loading-3';
 
 export const BREAKPOINTS = Object.freeze({
   mobileMax: 767,
