259544b5be6d9c6ceb722d21de2eb174b61ee5d7
tintinaccs <tintinaccs@gmail.com>
2026-09-01 15:07:41 -0300

    Corregir alta de usuario y logo de login

--- js/pages/login/mantenimiento-acceso.js
+++ js/pages/login/mantenimiento-acceso.js
@@ -25,7 +25,7 @@ if (LOGIN_RE.test(location.pathname || '') && !window.TintinLoginMaintenanceBoot
     );
     ensureStyle(
       'link[data-tt-login-onboarding-flow]',
-      '../../../css/pages/login/login-onboarding-flow.css?v=tintin-20260901-login-session-visual-1',
+      '../../../css/pages/login/login-onboarding-flow.css?v=tintin-20260901-login-session-visual-2',
       'ttLoginOnboardingFlow'
     );
   }
