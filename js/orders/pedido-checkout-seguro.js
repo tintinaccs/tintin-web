7aba757e688d488a80fbf247fee420afe427ed83
tintinaccs <tintinaccs@gmail.com>
2026-09-03 14:02:33 -0300

    chore(cache): bump canonical order client version

--- js/orders/pedido-checkout-seguro.js
+++ js/orders/pedido-checkout-seguro.js
@@ -28,7 +28,7 @@ import {
   normalizeRuc,
   isValidRazonSocial
 } from '../components/forms/validacion-documentos-py.js?v=tintin-20260822-facturacion-1';
-import { createOrderViaServer } from '../create-order-client.js?v=tintin-20260822-checkout-hardening-2';
+import { createOrderViaServer } from '../create-order-client.js?v=tintin-20260903-canonical-order-bridge-1';
 import { composeCheckoutDraft } from './politica-checkout.js?v=tintin-20260822-checkout-hardening-2';
 
 if (!window.TintinSecureCheckoutOrderBooted) {
