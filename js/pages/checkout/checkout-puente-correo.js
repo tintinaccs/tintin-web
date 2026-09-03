0103c587005111567404e5a352b865361c500baf
tintinaccs <tintinaccs@gmail.com>
2026-09-03 16:19:18 -0300

    fix: invalidate cached Firebase bootstrap (#659)

--- js/pages/checkout/checkout-puente-correo.js
+++ js/pages/checkout/checkout-puente-correo.js
@@ -1,4 +1,4 @@
-import { auth, db } from '../../core/firebase/firebase.js?v=tintin-20260903-app-check-singleton-3';
+import { auth, db } from '../../core/firebase/firebase.js?v=tintin-20260903-app-check-singleton-4';
 import {
   collection,
   query,
