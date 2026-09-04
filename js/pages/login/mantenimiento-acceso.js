58bbd1768987b4bb2f78ef6780ac52dba1dd7cf8
tintinaccs <tintinaccs@gmail.com>
2026-09-03 21:06:24 -0300

    fix: align login surface cache tag

--- js/pages/login/mantenimiento-acceso.js
+++ js/pages/login/mantenimiento-acceso.js
@@ -25,7 +25,7 @@ if (LOGIN_RE.test(location.pathname || '') && !window.TintinLoginMaintenanceBoot
     );
     ensureStyle(
       'link[data-tt-login-onboarding-flow]',
-      '../../../css/pages/login/login-onboarding-flow.css?v=tintin-20260903-login-surface-1',
+      '../../../css/pages/login/login-onboarding-flow.css?v=tintin-20260904-login-surface-no-card-1',
       'ttLoginOnboardingFlow'
     );
   }
