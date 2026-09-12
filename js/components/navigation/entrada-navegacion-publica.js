47d29075bc89a287a5577bc749e18e7749eb4efb
tintinaccs <tintinaccs@gmail.com>
2026-09-12 09:37:18 -0300

    chore(cache): versionar carga-navegacion actualizado

--- js/components/navigation/entrada-navegacion-publica.js
+++ js/components/navigation/entrada-navegacion-publica.js
@@ -8,7 +8,7 @@ import { renderCollectionsSheet } from './compartido/panel-colecciones.js';
 import { renderSurfaceLayer } from './compartido/capas-paneles.js';
 import { applyActiveState, currentPage } from './compartido/estado-ruta.js';
 import { ensureNavigationAssets } from './compartido/recursos-navegacion.js?v=tintin-20260909-unified-navigation-assets-3';
-import { loadProductsRuntime, loadSharedRuntime } from './compartido/carga-navegacion.js?v=tintin-20260909-unified-navigation-assets-3';
+import { loadProductsRuntime, loadSharedRuntime } from './compartido/carga-navegacion.js?v=tintin-20260912-login-checkout-e2e-4';
 import { enhanceMobileFooter } from './compartido/acordeon-pie-pagina.js';
 import { registerNavigationSurfaces } from './compartido/registro-paneles.js';
 import { fetchGlobalVisualStudioConfig, applyGlobalLayout } from './compartido/apariencia-global.js?v=tintin-20260817-footer-contrast-1';
