5a9c7e9ac7d5702fbf3d9377c051623471846617
tintinaccs <tintinaccs@gmail.com>
2026-09-12 09:30:37 -0300

    chore(cache): versionar estado-productos actualizado

--- js/components/navigation/compartido/control-busqueda.js
+++ js/components/navigation/compartido/control-busqueda.js
@@ -244,7 +244,7 @@ async function ensureProducts(force = false) {
     results.replaceChildren(stateNode('Cargando catálogo…'));
   }
 
-  loadPromise = import('../../../core/store/estado-productos.js?v=tintin-20260908-public-activity-auth-2')
+  loadPromise = import('../../../core/store/estado-productos.js?v=tintin-20260912-login-checkout-e2e-2')
     .then(module => {
       const load = window.TintinProductsStore?.ensureSearch || module.ensureProductsForSearch || module.loadAllProducts;
       return typeof load === 'function' ? load({ force }) : window.PRODUCTS || [];
