76b7ec723b0878a5295f9adfa853cb71b74b42ea
tintinaccs <tintinaccs@gmail.com>
2026-09-15 13:07:24 -0300

    Polish public card surfaces and remove inactive runtime

--- js/components/welcome/tutorial-bienvenida.js
+++ js/components/welcome/tutorial-bienvenida.js
@@ -1,7 +1,7 @@
 import { auth, db, appCheckReady } from '../../core/firebase/firebase.js?v=tintin-20260908-admin-cache-reset-1';
 import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js';
 import { doc, getDoc, setDoc, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js';
-import { getUserRole, SUPER_ADMIN } from '../../core/auth/roles.js?v=tintin-20260821-accounts-phase-a-3';
+import { getUserRole, SUPER_ADMIN } from '../../core/auth/roles.js?v=tintin-20260915-final-polish-1';
 import {
   DEFAULT_WELCOME_STEPS,
   WELCOME_VERSION,
