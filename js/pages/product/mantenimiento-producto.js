3eb48dc91603e93a54fae9da3fe370f66366ac43
tintinaccs <tintinaccs@gmail.com>
2026-09-21 21:18:56 -0300

    chore(product): bump maintenance CSS cache version

--- js/pages/product/mantenimiento-producto.js
+++ js/pages/product/mantenimiento-producto.js
@@ -60,7 +60,7 @@ if (isProductPage() && !window.TintinProductMaintenanceBooted) {
     if (document.querySelector('link[data-tt-product-maintenance]')) return;
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = new URL('../../../css/pages/product/product-maintenance.css?v=tintin-20260817-sync-badge-breadcrumb-1', import.meta.url).href;
+    link.href = new URL('../../../css/pages/product/product-maintenance.css?v=tintin-20260921-product-surface-white-1', import.meta.url).href;
     link.dataset.ttProductMaintenance = '1';
     document.head.appendChild(link);
   }
