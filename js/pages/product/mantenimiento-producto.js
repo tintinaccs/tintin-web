a4176cd675f803cbbbd755cbb8a1ffff87abce36
tintinaccs <tintinaccs@gmail.com>
2026-09-21 22:34:06 -0300

    chore(product): refresh final gallery cache

--- js/pages/product/mantenimiento-producto.js
+++ js/pages/product/mantenimiento-producto.js
@@ -61,7 +61,7 @@ if (isProductPage() && !window.TintinProductMaintenanceBooted) {
     if (document.querySelector('link[data-tt-product-maintenance]')) return;
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = new URL('../../../css/pages/product/product-maintenance.css?v=tintin-20260921-product-surface-white-6', import.meta.url).href;
+    link.href = new URL('../../../css/pages/product/product-maintenance.css?v=tintin-20260921-product-surface-white-7', import.meta.url).href;
     link.dataset.ttProductMaintenance = '1';
     document.head.appendChild(link);
   }
