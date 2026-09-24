af4ede27f9dac2c1af116f191a617bae160b87bc
tintinaccs <tintinaccs@gmail.com>
2026-09-24 15:14:24 -0300

    chore(cache): revert unrelated Firebase version-only change

--- js/pages/checkout/checkout-sincronizacion-nombre.js
+++ js/pages/checkout/checkout-sincronizacion-nombre.js
@@ -1,4 +1,4 @@
-import { auth } from '../../core/firebase/firebase.js?v=tintin-20260924-auth-persistence-stable-2';
+import { auth } from '../../core/firebase/firebase.js?v=tintin-20260924-auth-persistence-init-1';
 import { subscribeAuthState } from '../../core/auth/coordinador-sesion.js?v=tintin-20260924-auth-state-authority-1';
 
 (function () {
